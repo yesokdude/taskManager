2256731e35d24a4b15f2aa1fbf61b2061b966937
yesokdude <zarezju@gmail.com>
2020-06-29 16:45:08 +0300

    added task output

--- db.php
+++ db.php
@@ -5,19 +5,6 @@
 		$name_db = 'task_db';
 
 		$conn = mysqli_connect($host, $login, $password, $name_db);
-
-		if (isset($_POST['taskarea'])) {
-			$sql = "INSERT INTO task_table (task) VALUES ('{$_POST['taskarea']}')";
-			$query = mysqli_query($conn, $sql);
-		
-			if (query) {
-				echo "добавил в бд";
-			}else {
-				echo "не добавил";
-			}
-		}
-
-		$outquery = mysqli_query($conn, "SELECT * FROM task_table");
 	    	
 		
 	 ?>
--- index.php
+++ index.php
@@ -9,7 +9,10 @@
 </head>
 <body>
 	<h1>Я запомню все, что тебе нужно сделать сегодня.</h1>
+
+	<!--Форма и кнопка-->
 	<div class="container">
+
 		<form method="POST" action="">
 			<input type="text" name="taskarea" class="form-control" id="form-control" placeholder="Введите задачу...">
 		<button class="btn btn-success">Отправить</button>
@@ -17,8 +20,34 @@
 		
 	</div>
 
-	
-	<?php include_once 'db.php' ?>
+	<!--Получение данных с формы и отправка в БД. -->
+	<?php include_once 'db.php';
+		
+		if (isset($_POST['taskarea']) && ($_POST['taskarea']) != null) {
+			$sql = "INSERT INTO task_table (task) VALUES ('{$_POST['taskarea']}')";
+			$query = mysqli_query($conn, $sql);
+			
+		
+		}
+
+		
+
+	?>
+	<h6>Список задач:</h6>
+
+	<div class="taskList">
+		<?php 
+
+		$queryTaskList = "SELECT * FROM task_table";
+		$result = mysqli_query($conn, $queryTaskList) or die('Ошибка вывода');
+		
+		while ($row = $result->fetch_assoc()) {
+			echo $row['task'].'<br/>';
+
+		}
+
+		?>
+	</div>
 
 
 </body>
